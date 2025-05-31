84222aaa833cfae21ac808857b490329693ead68
Gustaf Delport <gustaf.delport@standardbank.co.za>
2025-05-31 15:31:09 +0200

    Add Ma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../components/shared/navigation/navigation.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [
+      NavigationComponent //
+    ]
 })
 export class AppComponent {
     title = 'GnuHiveApp';
