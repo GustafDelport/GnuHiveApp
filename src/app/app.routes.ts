34551caf8bd061b513d56cd99fb4cefbbf303016
Gustaf Delport <gustaf.delport@standardbank.co.za>
2025-05-31 15:22:06 +0200

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from '../components/shared/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: NotFoundComponent
+  },
+  { path: '**', component: NotFoundComponent }
+];
